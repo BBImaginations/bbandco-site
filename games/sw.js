0169df79ea37c5da238cf4832629321d1dba08b3
BBImaginations <admin@bbandcoimaginations.co.uk>
2026-09-07 21:16:19 +0100

    Add files via upload

--- games/sw.js
+++ games/sw.js
@@ -1,4 +1,4 @@
-const CACHE='whowins-v2';
+const CACHE='whowins-v3';
 const CORE=['./who-wins.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png','./icons/apple-touch-icon.png','../assets/logo-wide.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
