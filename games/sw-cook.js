4d1084f0a132cdc19e6cea35a92f20f5ec68ea36
BBImaginations <admin@bbandcoimaginations.co.uk>
2026-09-10 13:41:22 +0100

    Add files via upload

--- games/sw-cook.js
+++ games/sw-cook.js
@@ -1,4 +1,4 @@
-const CACHE='littlecook-v3';
+const CACHE='littlecook-v4';
 const CORE=['./little-cook.html','./little-cook.webmanifest','./icons/cook-192.png','./icons/cook-512.png','./icons/cook-180.png','../assets/logo-wide.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
